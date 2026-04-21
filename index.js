9f9259a6fd06d6d72d272ba00ff62e5faf762e85
Julian <figueredo.julian96@gmail.com>
2026-04-21 02:02:40 -0500

    Actualizacion PORT

--- index.js
+++ index.js
@@ -105,6 +105,8 @@ app.post('/api/beneficio/evaluar', (req, res) => {
   }, delay);
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto http://localhost:${PORT}`);
 });
